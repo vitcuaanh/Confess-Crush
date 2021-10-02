c8be79b93f253718e1edf1d641c25d70729245fc
Tenz02 <91783421+Tenz02@users.noreply.github.com>
2021-10-02 18:52:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Hello Băng béo",
-    introTitle: 'Nguyễn Vũ Băng Vy',
+    titleWeb: "chào các bạn",
+    introTitle: 'hello các bạn',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'tao làm ra cái này đấy 🥰',
+    desc: 'đỉnh vãi',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'anh huy là số 1 đúng không',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'đúng vậy <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'cho tao vay tiền nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100034824795446' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
