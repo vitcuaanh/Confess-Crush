65a24f4fb0280d7ef8c8efc1ec7afc40171528b5
Tenz02 <91783421+Tenz02@users.noreply.github.com>
2021-10-04 11:59:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "chào các bạn",
-    introTitle: 'hello các bạn',
+    introTitle: 'Hi bae <3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
